e77f4af8442b296687d7a07b24d2aef8288ac5e7
sejalfarkade29 <150161334+sejalfarkade29@users.noreply.github.com>
2023-12-30 22:33:03 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the number a\n");
+    scanf("%d",&a);
+
+    printf("Enter the number b\n");
+    scanf("%d",&b);
+
+    printf("The number is %d\n",a+b);
+
+    return 0;
+}
