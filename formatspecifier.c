e6c62c2d6a694c98647b39fdeee93fa9a5ad469a
sejalfarkade29 <150161334+sejalfarkade29@users.noreply.github.com>
2023-12-31 13:13:06 +0530

    Add files via upload

---
+++ formatspecifier.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=8;
+    float b=4.988;
+    printf("Hello World\n");
+    printf("The value of a is%d\nThe value of b is %9.14f\n",a,b);
+    return 0;
+}
